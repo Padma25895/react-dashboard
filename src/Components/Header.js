6940dfd4edbb40dc2c9779ec895c726c77098c3b
Padma <25895padma@gmail.com>
2024-08-07 11:27:42 +0530

    Updated the deployment file with new configurations

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -40,6 +40,10 @@ const Header = () => {
                 </div>
             </li>
           </ul>
+          <button className="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button"
+            data-toggle="offcanvas">
+            <span className="mdi mdi-format-line-spacing"></span>
+          </button>
         </div>
       </nav>
     </div>
